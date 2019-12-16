5f89bd2d350e28afba67b07d9a779cb336cdc905
Martin Schröder <m.schroeder@tu-berlin.de>
2019-12-16 16:52:18 +0100

    updeded version

--- pyscada/scripting/__init__.py
+++ pyscada/scripting/__init__.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 import pyscada
 
-__version__ = '0.7.0rc9'
+__version__ = '0.7.0'
 __author__ = 'Martin Schröder'
 
 default_app_config = 'pyscada.scripting.apps.PyScadaScriptingConfig'
